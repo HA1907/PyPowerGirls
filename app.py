e89938a6b7df312c7593a60d6872fe200e2cbf80
HA1907 <32873469+HA1907@users.noreply.github.com>
2017-11-29 19:13:05 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask("demoApp")
+
+@app.route("/")
+def say_hello():
+	return "lol"
+
+@app.route("/<name>")
+def say_hello_with_name(name):
+	return "Hello {}".format (name)
+
+@app.route("/hello/<name>")
+def say_hello_the_old_way_with_name(name):
+	return render_template ("index.html", name=name)
+
+@app.route("another.html")	
+
+
+if 'PORT' in os.environ:
+     app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+else:
+     app.run(debug=True)
